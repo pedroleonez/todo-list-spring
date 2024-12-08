627df85d5e3b9682f94c6a5e76772faddfc02a0b
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2024-12-08 18:43:20 -0300

    tests

--- src/main/java/com/example/todo_list/controller/TodoController.java
+++ src/main/java/com/example/todo_list/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.example.todo_list.controller;
 
 import com.example.todo_list.entity.Todo;
 import com.example.todo_list.service.TodoService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,7 +17,7 @@ public class TodoController {
     }
 
     @PostMapping
-    List<Todo> create(@RequestBody Todo todo) {
+    List<Todo> create(@RequestBody @Valid Todo todo) {
         return todoService.create(todo);
     }
 
