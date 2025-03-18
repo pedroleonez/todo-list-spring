c5621b120666831a8600daeb18c2550023ab1552
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-03-18 14:06:56 -0300

    refactor: add lombok and change all to english

--- src/main/java/com/example/todo_list/entity/Todo.java
+++ src/main/java/com/example/todo_list/entity/Todo.java
@@ -2,65 +2,27 @@ package com.example.todo_list.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "todos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Todo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
-    private String nome;
+    private String name;
     @NotBlank
-    private String descricao;
-    private boolean realizado;
-    private int prioridade;
-
-    public Todo(String nome, String descricao, boolean realizado, int prioridade) {
-        this.nome = nome;
-        this.descricao = descricao;
-        this.realizado = realizado;
-        this.prioridade = prioridade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public boolean isRealizado() {
-        return realizado;
-    }
-
-    public void setRealizado(boolean realizado) {
-        this.realizado = realizado;
-    }
-
-    public int getPrioridade() {
-        return prioridade;
-    }
-
-    public void setPrioridade(int prioridade) {
-        this.prioridade = prioridade;
-    }
+    private String description;
+    private boolean done;
+    @Column
+    private int priority;
 }
