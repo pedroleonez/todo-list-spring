443f0f737ae7b65ee4b2b609923aa612ec0e0481
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-27 21:39:56 -0300

    refactor: change todos to tasks

--- src/main/java/com/example/todo_list/controller/TaskController.java
+++ src/main/java/com/example/todo_list/controller/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/todos")
+@RequestMapping("/api/tasks")
 public class TaskController {
     private final TaskService taskService;
 
