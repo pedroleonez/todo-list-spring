e566f1633c5ea4c253f8d2fc1d646dfed2bb27f2
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-06 21:55:01 -0300

    feat: add Min and Max validations to priority

--- src/main/java/com/example/todo_list/dtos/input/CreateTaskDto.java
+++ src/main/java/com/example/todo_list/dtos/input/CreateTaskDto.java
@@ -2,6 +2,8 @@ package com.example.todo_list.dtos.input;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
 
 public record CreateTaskDto(
         @NotBlank
@@ -11,5 +13,7 @@ public record CreateTaskDto(
         String description,
 
         @NotNull
+        @Min(1)
+        @Max(3)
         Integer priority) {
 }
--- src/main/java/com/example/todo_list/dtos/input/UpdateTaskDto.java
+++ src/main/java/com/example/todo_list/dtos/input/UpdateTaskDto.java
@@ -2,6 +2,8 @@ package com.example.todo_list.dtos.input;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
 
 public record UpdateTaskDto(
         @NotBlank
@@ -13,5 +15,7 @@ public record UpdateTaskDto(
         boolean completed,
 
         @NotNull
+        @Min(1)
+        @Max(3)
         Integer priority) {
 }
