a2e8bb9b761d94ecfe3bbbd22e6feffe5998233d
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-06 21:47:54 -0300

    feat: create and impl TaskNotFoundException

---
+++ src/main/java/com/example/todo_list/exceptions/TaskNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.todo_list.exceptions;
+
+public class TaskNotFoundException extends RuntimeException {
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+
+}
